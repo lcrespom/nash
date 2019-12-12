729d226607c16f907ce7e7efb8adfc7d4bea756a
Luis Crespo <lcrespom@gmail.com>
2019-12-12 09:46:32 +0100

    About to parse params

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,54 @@
+const ParseMode = {
+	command: 1,
+	param: 2,
+	squotes: 3,
+	dquotes: 4,
+	javascript: 5
+}
+
+function isSeparator(ch) {
+	return ch == ' ' || ch == '\t' || ch == '\n'
+}
+
+function skipSeparators(pos, line) {
+	while (isSeparator(line[pos]) && pos < line.length) {
+		pos++
+	}
+	return pos
+}
+
+function readWord(pos, line) {
+	let word = ''
+	pos = skipSeparators(pos, line)
+	while (!isSeparator(line[pos]) && pos < line.length) {
+		word += line[pos]
+		pos++
+	}
+	pos = skipSeparators(pos, line)
+	return [pos, word]
+}
+
 function parse(line) {
+	let mode = ParseMode.command
+	let pos = 0
+	let command = null
+	let param = null, params = []
+	while (pos < line.length) {
+		switch (mode) {
+			case ParseMode.command:
+				[pos, command] = readWord(pos, line)
+				mode = ParseMode.param
+				break
+			case ParseMode.param:
+				[pos, param] = readParam(pos, line)
+				break
+			default:
+				throw new Error(`Unexpected parse mode: ${mode}`)
+		}
+	}
 	return {
-		command: line,
-		params: []
+		command,
+		params
 	}
 }
 
--- test/parse.test.js
+++ test/parse.test.js
@@ -1,6 +1,11 @@
 const parser = require('../src/parser');
 
 test('Single command', () => {
-	let pc = parser.parse('ls')
-	expect(pc.command).toBe('ls');
-});
+	expect(parser.parse('ls').command).toBe('ls');
+})
+
+test('Single command with blanks', () => {
+	expect(parser.parse('  ls').command).toBe('ls');
+	expect(parser.parse('ls   ').command).toBe('ls');
+	expect(parser.parse(' \t ls \t').command).toBe('ls');
+})
